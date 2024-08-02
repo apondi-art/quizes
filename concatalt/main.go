fa63db0ae400506d198412b8e7f5d758532bb00d
quochieng <aiyanarich64@gmail.com>
2024-08-02 15:30:36 +0300

    added some feature to func alconternate

--- concatalt/main.go
+++ concatalt/main.go
@@ -3,36 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(ConcatAlternate([]int{4, 5, 6, 1, 2, 3}, []int{1, 3, 7, 8, 10, 4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
 }
 
-func ConcatAlternate(t, s []int) []int {
+func ConcatAlternate(slice1, slice2 []int) []int {
 	var nb []int
-	if len(t) > len(s) {
-		out := len(t) - len(s)
-		nb = append(nb, t[:out]...)
-		for i := 0; i < len(s); i++ {
-			nb = append(nb, t[out+i])
-			nb = append(nb, s[i])
+	if len(slice1) > len(slice2) {
+		for i := 0; i < len(slice1); i++ {
+			if i > len(slice2)-1 {
+				nb = append(nb, slice1[i])
+			} else {
+				nb = append(nb, slice1[i])
+				nb = append(nb, slice2[i])
 
+			}
 		}
 		return nb
 
-	} else if len(s) > len(t) {
-		out := len(s) - len(t)
-		nb = append(nb, s[:out]...)
-		for i := 0; i < len(t); i++ {
-			nb = append(nb, s[out+i])
-			nb = append(nb, t[i])
+	} else if len(slice2) > len(slice1) {
+		for i := 0; i < len(slice2); i++ {
+			if i > len(slice1)-1 {
+				nb = append(nb, slice2[i])
+			} else {
+				nb = append(nb, slice2[i])
+				nb = append(nb, slice1[i])
 
+			}
 		}
 		return nb
 
 	} else {
-		times := len(s)
+		times := len(slice2)
 		for i := 0; i < times; i++ {
-			nb = append(nb, s[i])
-			nb = append(nb, t[i])
+			nb = append(nb, slice1[i])
+			nb = append(nb, slice2[i])
 
 		}
 		return nb
